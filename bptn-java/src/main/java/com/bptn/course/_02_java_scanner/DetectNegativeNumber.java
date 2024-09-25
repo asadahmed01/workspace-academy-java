a9b948340d3a8add63ad0176452c18280fb0cfec
=asadahmed01 <asadatnish@gmail.com>
2024-09-25 07:48:15 -0600

    initial commit of the academy workspace files

---
+++ bptn-java/src/main/java/com/bptn/course/_02_java_scanner/DetectNegativeNumber.java
@@ -0,0 +1,10 @@
+package com.bptn.course._02_java_scanner;
+
+public class DetectNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bptn-java/src/main/java/com/bptn/course/_03_number_of_steps/StepsToZero.java
@@ -0,0 +1,10 @@
+package com.bptn.course._03_number_of_steps;
+
+public class StepsToZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
